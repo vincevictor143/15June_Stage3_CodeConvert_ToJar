36588d8981e7f70c80f935b73571c59f3e1be905
vincevictor143 <155403863+vincevictor143@users.noreply.github.com>
2024-09-19 08:23:11 +0530

    Update AppRunnerClass.java
    Added the code of Cucumber Report Generation

--- src/test/java/appfeaturesStef/AppRunnerClass.java
+++ src/test/java/appfeaturesStef/AppRunnerClass.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 (
 		features={"src\\test\\resources\\appfeatures"},
 		glue={"appfeaturesStef"},
-		plugin={"pretty"}
+		plugin={"pretty","html:target/cucumber-reports.html"}
 	
 )
 
